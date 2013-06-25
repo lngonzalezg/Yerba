ec4700727bcaab2bf9c9519126ab563b3b644a46
Evan Briones <evrick@email.arizona.edu>
2013-06-25 12:00:06 -0700

    Added ChainMap to support multiple configuration options.

--- yerba/utils.py
+++ yerba/utils.py
@@ -1,4 +1,6 @@
 from contextlib import contextmanager
+import UserDict
+
 @contextmanager
 def ignored(*exceptions):
     '''
@@ -8,3 +10,15 @@ def ignored(*exceptions):
         yield
     except exceptions:
         pass
+
+class ChainMap(UserDict.DictMixin):
+    def __init__(self, *maps):
+        self._maps = maps
+
+    def __getitem__(self, key):
+        for mapping in self._maps:
+            with ignored(KeyError):
+                item = mapping[key]
+                return item
+
+        raise KeyError(key)
