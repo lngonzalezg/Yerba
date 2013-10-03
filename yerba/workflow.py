3fc0b9ea8c5dca138fa32551e2c93d3239b370b8
Evan Briones <erbriones@gmail.com>
2013-10-03 10:59:31 -0700

    All jobs are now logged after a workflow has been completed.

--- yerba/workflow.py
+++ yerba/workflow.py
@@ -31,7 +31,7 @@ class Job(object):
         self.retries = retries
         self._status = 'waiting'
         self._description = description
-        self._logged = False
+        self._info = None
 
     @property
     def status(self):
@@ -40,6 +40,14 @@ class Job(object):
     @status.setter
     def status(self, value):
         self._status = value
+
+    @property
+    def info(self):
+        return self._info
+
+    @info.setter
+    def info(self, info):
+        self._info = info
 
     @property
     def description(self):
@@ -66,20 +74,6 @@ class Job(object):
     def failed(self):
         return self.retries <= 0
 
-    def log(self, log):
-        with open(log, 'a') as fp:
-            if self._logged:
-                return
-
-            if self._status == 'skipped':
-                fp.write('#' * 25 + '\n')
-                fp.write('{0}\n'.format(self.description))
-                fp.write("Job: %s\n" % str(self))
-                fp.write("Skipped: The analysis was previously generated.\n")
-                fp.write('#' * 25 + '\n\n')
-
-        self._logged = True
-
     def __eq__(self, other):
         return (sorted(other.inputs) == sorted(self.inputs) and
                 sorted(other.outputs) == sorted(self.outputs) and
@@ -99,13 +93,6 @@ class WorkflowHelper(object):
     def workflow(self):
         return self._workflow
 
-    def log(self):
-        '''
-        Logs the results of workflow.
-        '''
-        for job in self.workflow.jobs:
-            job.log(self.workflow.log)
-
     def waiting(self):
         '''
         Return the set of jobs waiting to be scheduled.
@@ -123,6 +110,45 @@ class WorkflowHelper(object):
         Return the set of jobs that are ready
         '''
         return {job for job in self._workflow.jobs if job.ready()}
+
+    def add_job_info(self, selected, info):
+        '''
+        Adds job information for the selected job
+        '''
+        for job in self._workflow.jobs:
+            if job == selected:
+                job.info = info
+
+    def log(self):
+        '''
+        Logs the results of workflow.
+        '''
+        if self._workflow._logged:
+            return
+
+        self._workflow._logged = True
+
+        with open(self._workflow.log, 'a') as fp:
+            for job in self._workflow.jobs:
+                if job.status == 'skipped':
+                    fp.write('#' * 25 + '\n')
+                    fp.write('{0}\n'.format(job.description))
+                    fp.write("Job: %s\n" % str(job))
+                    fp.write("Skipped: The analysis was previously generated.\n")
+                    fp.write('#' * 25 + '\n\n')
+                elif job.info:
+                    msg = ("Job: {cmd}\n"
+                        "Submitted at: {started}\n"
+                        "Completed at: {ended}\n"
+                        "Execution time: {elapsed} sec\n"
+                        "Assigned to task: {taskid}\n"
+                        "Return status: {returned}\n"
+                        "{output}")
+
+                    fp.write('#' * 25 + '\n')
+                    fp.write('{0}\n'.format(job.description))
+                    fp.write(msg.format(**job.info))
+                    fp.write('#' * 25 + '\n\n')
 
     def status(self):
         '''
@@ -146,6 +172,7 @@ class Workflow(object):
         self._log = log
         self._priority = priority
         self._jobs = jobs
+        self._logged = False
 
     @property
     def jobs(self):
